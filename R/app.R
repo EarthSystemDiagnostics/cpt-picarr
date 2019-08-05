1d90f1cb76cf9612e084f3e85f6d4c3dc9d777b9
twollnik <tom.wollnik@student.hpi.de>
2019-08-05 10:14:47 +0200

    add landing page

--- R/app.R
+++ R/app.R
@@ -1,7 +1,27 @@
 library(shiny)
 
 ui <- fluidPage(
-  tags$h1("Welcome to Cpt. Picarr")
+  navlistPanel(
+    "Welcome to Cpt. Picarr!",
+    tabPanel("Select a project",
+            wellPanel(
+             h3("Load an existing project"),
+             selectInput("project_to_load", "Choose a project", c("Project A", "Project B", "Project C")),
+             actionButton("load_project", "Load selected project")
+            ),
+            wellPanel(
+             h3("Create a new project"),
+             textInput("proj_name", "Project name"),
+             textInput("proj_people", "People involved"),
+             textInput("proj_additional_info", "Additional info"),
+             dateInput("proj_date", "Expedition date"),
+             actionButton("create_project", "Create new project")
+            )
+    ),
+    tabPanel("Generate a Bestückungsprotokoll", "contents"),
+    tabPanel("Upload measurement data", "contents"),
+    tabPanel("Process measurement data", "contents")
+  )
 )
 
 server <- function(input, output){}
