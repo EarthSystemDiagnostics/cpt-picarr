be0fe88c5f233f21773d4e640d388a54d98df251
twollnik <tom.wollnik@student.hpi.de>
2019-08-21 14:39:28 +0200

    include column Tray in assembly protocol

--- R/global.R
+++ R/global.R
@@ -4,6 +4,7 @@ blue <- "color: #fff; background-color: #337ab7; border-color: #2e6da4"
 df_assembly_prot_template <- data.frame(
   id_1 = letters[1:20],
   id_2 = LETTERS[1:20],
+  tray = 1,
   num_injections = as.integer(3),
   is_standard = FALSE,
   standard_oxy_18 = "",
@@ -11,7 +12,9 @@ df_assembly_prot_template <- data.frame(
   standard_oxy_17 = "",
   stringsAsFactors = FALSE
 )
-colnames(df_assembly_prot_template) <- c("Identifier 1", "Identifier 2", "Number of injections", "Is standard?", "True d18O (only for standards)", "True d2H (only for standards)", "True d17O (only for standards, optional)")
+colnames(df_assembly_prot_template) <- c("Identifier 1", "Identifier 2", "Tray", "Number of injections", 
+                                         "Is standard?", "True d18O (only for standards)", "True d2H (only for standards)", 
+                                         "True d17O (only for standards, optional)")
 
 
 df_processing_template <- data.frame(
@@ -23,4 +26,5 @@ df_processing_template <- data.frame(
   use_as_control_standard = FALSE,
   stringsAsFactors = FALSE
 )
-colnames(df_processing_template) <- c("Identifier 1", "Identifier 2", "Use for memory correction?", "Use for drift correction?", "Use for calibration?", "Use as control standard?")
+colnames(df_processing_template) <- c("Identifier 1", "Identifier 2", "Use for memory correction?", 
+                                      "Use for drift correction?", "Use for calibration?", "Use as control standard?")
