aecb9cfc8e3d24603307464506cd11b5e8052d13
twollnik <tom.wollnik@student.hpi.de>
2019-08-27 12:01:19 +0200

    don't run shinytest for now

--- tests/testthat/testShinyApp.R
+++ tests/testthat/testShinyApp.R
@@ -2,8 +2,8 @@ library(shinytest)
 
 context("test the application in the app/ directory")
 
-
-test_that("cpt picarr works", {
-  
-  expect_pass(testApp("app/", compareImages = FALSE))
-})
+# test removed for now, because is slows down iteration
+# test_that("cpt picarr works", {
+#  
+#   expect_pass(testApp("app/", compareImages = FALSE))
+# })
