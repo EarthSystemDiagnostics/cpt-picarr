1a7d586bf958e517440d078520e069c75a8cfc17
twollnik <tom.wollnik@student.hpi.de>
2019-08-28 10:34:12 +0200

    add test for getUniqueIdentifier

---
+++ tests/testthat/testGetUniqueIdentifier.R
@@ -0,0 +1,21 @@
+library(testthat)
+
+context("test function getUniqueIdentifier")
+
+test_that("test", {
+  
+  data1 <- tribble(
+    ~colA, ~colB,
+    1,     2
+  )
+  data2 <- tribble(
+    ~colA, ~colB,
+    3,     4
+  )
+  
+  id1 <- getUniqueIdentifer(data1)
+  id2 <- getUniqueIdentifer(data2)
+  
+  expect_type(id1, "character")
+  expect_true(id1 != id2)
+})
