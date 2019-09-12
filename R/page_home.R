12ab77ca025de92a1f531faf98aaa5b4576f3f3a
twollnik <tom.wollnik@student.hpi.de>
2019-09-12 20:53:25 +0200

    move helpers to pages that use them

--- R/page_home.R
+++ R/page_home.R
@@ -120,3 +120,26 @@ setProject <- function(newProject, envir){
   assign(".newProject", newProject, envir = envir)
   evalq(rv$project <- .newProject, envir = envir)
 }
+
+projectExistsAlready <- function(projectName, basePath = BASE_PATH){
+  dir.exists(file.path(basePath, projectName))
+}
+
+createProjectDirectory <- function(projectName, basePath = BASE_PATH){
+  dir.create(file.path(basePath, projectName))
+}
+
+createProjectInfoFile <- function(input, basePath = BASE_PATH){
+  
+  projectName <- input$projectName
+  
+  projectInfo <- list(
+    name = projectName,
+    people = input$projectPeople,
+    additionalInfo = input$projectAdditionalInfo,
+    date = input$projectDate
+  )
+  
+  filePath <- file.path(basePath, projectName, "projectInfo.json")
+  rlist::list.save(projectInfo, filePath)
+}
--- R/page_project.R
+++ R/page_project.R
@@ -187,60 +187,40 @@ loadProjectInfo <- function(projectName, basePath = BASE_PATH){
   rlist::list.load(path)
 }
 
-#' Get a List with relevant information about the project. 
-#' 
-#' Relies on getStoredData(..).
-#' 
-#' @return A named list. The names correspond to the names of the datsets 
-#'         for the given project. Has the following components:
-#'           $path (path to the folder with data for this dataset)
-#'           $raw  (name of the file with raw data)
-#'           $date (measurement date)
-#'           $additionalInfo (content of the file 'fileInfo.txt')
-#'           $processed ('processed.csv' if the dataset has been processed)
-#'         Note that only the components are present, for which data
-#'         exists.
-loadProjectData <- function(project, basePath = BASE_PATH){
+downloadProcessedDataAsZip <- function(project, outputFile, basePath = BASE_PATH){
   
-  projectDataPath <- file.path(basePath, project, "data")
-  flog.debug("loading data info: %s", projectDataPath)
+  # temp dir to store processed data for zipping
+  temp <- "tempDirForZipping"
+  unlink(temp, recursive = TRUE)
+  dir.create(temp)
+  on.exit(unlink(temp, recursive = TRUE))
   
-  datasetNames <- list.files(projectDataPath, recursive = FALSE)
+  projDataPath <- file.path(basePath, project, "data")
+  datasetNames <- list.dirs(projDataPath, recursive = FALSE, full.names = FALSE)
   
-  projectData <- map(datasetNames, getStoredData, 
-                     project = project, basePath = basePath)
-  names(projectData) <- datasetNames
-  return(projectData)
+  walk(datasetNames, ~ {
+    processedFile <- file.path(projDataPath, ., "processed.csv")
+    if(file.exists(processedFile)){
+      path <- file.path(temp, if(endsWith(., ".csv")) . else str_c(., ".csv"))
+      file.copy(processedFile, path)
+    }
+  })
+  
+  filesToZip <- list.files(temp, full.names = TRUE)
+  zip(outputFile, filesToZip, flags = "-r9Xj")
 }
 
-getStoredData <- function(datasetName, project, basePath){
+downloadProcessedDataSingleFile <- function(project, outputFile, basePath = BASE_PATH){
   
-  projectDataPath <- file.path(basePath, project, "data")
+  projDataPath <- file.path(basePath, project, "data")
+  processedDatasets <- list.files(
+    projDataPath, pattern = "*processed.csv", full.names = TRUE, recursive = TRUE)
+  datasetNames <- map_chr(processedDatasets, ~ str_extract(., "(?<=/)[^/]+(?=/processed.csv)"))
   
-  # use to assemble output step-by-step
-  output <- list(
-    path = file.path(projectDataPath, datasetName)
-  )
+  allProcessedData <- map(processedDatasets, read_csv) %>%
+    map2(., datasetNames, ~ {.x$dataset <- .y; .x}) %>%
+    do.call(rbind, .) %>%
+    data.frame()
   
-  # add rawFile and date to output
-  rawFile <- getPathToRawData(datasetName, project, basePath, fullPath = FALSE)
-  if (!is_empty(rawFile)){
-    rawData <- read_csv(file.path(projectDataPath, datasetName, rawFile))
-    date <- lubridate::ymd_hms(rawData$`Time Code`) %>%
-      lubridate::date() %>%
-      first() %>%
-      as.character()
-    output$raw <- rawFile
-    output$date <- date
-  }
-  
-  # add additionalInfo to output
-  infoFile <- file.path(projectDataPath, datasetName, "fileInfo.txt")
-  if(file.exists(infoFile)) output$additionalInfo <- read_file(infoFile)
-  
-  # add processed to output
-  processedFile <- file.path(projectDataPath, datasetName, "processed.csv")
-  if(file.exists(processedFile)) output$processed <- "processed.csv"
-  
-  return(output)
+  write_csv(allProcessedData, outputFile)
 }
