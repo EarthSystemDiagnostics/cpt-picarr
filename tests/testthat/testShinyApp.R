c6a9241dae62b60569cc09225f1845efbba3da63
twollnik <tom.wollnik@student.hpi.de>
2019-09-21 15:37:39 +0200

    remove unused test file

--- tests/testthat/testShinyApp.R
+++
@@ -1,9 +0,0 @@
-library(shinytest)
-
-context("test the application in the app/ directory")
-
-# test removed for now, because is slows down iteration
-# test_that("cpt picarr works", {
-#  
-#   expect_pass(testApp("app/", compareImages = FALSE))
-# })
